42ce428041d26300107d0daf6938ccd2025b14c6
wangbingzhigit <853244870@qq.com>
2017-09-29 10:52:36 +0800

    crm system

--- src/com/crm/action/UserAction.java
+++ src/com/crm/action/UserAction.java
@@ -1,7 +1,10 @@
 package com.crm.action;
 
+import com.opensymphony.xwork2.ActionSupport;
+
 /**
  * Created by 王炳智 on 2017/9/28.
  */
-public class UserAction {
+public class UserAction extends ActionSupport{
+
 }
